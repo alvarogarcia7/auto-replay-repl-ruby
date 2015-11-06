1a087fd51d9cf7e9480399ed55f5e218071f075b
Alvaro Garcia <derecerca@gmail.com>
2015-11-06 19:24:13 +0100

    wrap function in a class

--- replayer.rb
+++ replayer.rb
@@ -1,21 +1,23 @@
-def execute_all_in file
-  lines = [];
+class Replayer
+  def self.execute_all_in file
+    lines = [];
 
-  File.foreach( file ) do |line|
-    lines << "#{line}"
-  end
-
-  result = {}
-  line_number = 1
-  lines.map do |line|
-    begin
-      r = eval(line)
-      result[line_number] = {error: false, result: r}
-    rescue StandardError => e
-      result[line_number] = {error: true, result: e}
+    File.foreach( file ) do |line|
+      lines << "#{line}"
     end
-    line_number = line_number + 1
-  end
 
-  result
+    result = {}
+    line_number = 1
+    lines.map do |line|
+      begin
+        r = eval(line)
+        result[line_number] = {error: false, result: r}
+      rescue StandardError => e
+        result[line_number] = {error: true, result: e}
+      end
+      line_number = line_number + 1
+    end
+
+    result
+  end
 end
