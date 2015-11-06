d847973297d10b7ecc01cde92a1bfb469f1cec1a
Alvaro Garcia <derecerca@gmail.com>
2015-11-06 19:21:51 +0100

    create simple version of the replayer

---
+++ replayer.rb
@@ -0,0 +1,21 @@
+def execute_all_in file
+  lines = [];
+
+  File.foreach( file ) do |line|
+    lines << "#{line}"
+  end
+
+  result = {}
+  line_number = 1
+  lines.map do |line|
+    begin
+      r = eval(line)
+      result[line_number] = {error: false, result: r}
+    rescue StandardError => e
+      result[line_number] = {error: true, result: e}
+    end
+    line_number = line_number + 1
+  end
+
+  result
+end
