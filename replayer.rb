b0f4c1b98e8616deb12edac00007a5bfaf54ca43
Alvaro Garcia <derecerca@gmail.com>
2015-11-06 19:42:11 +0100

    refactor: abstract how to execute all the lines

--- replayer.rb
+++ replayer.rb
@@ -2,19 +2,7 @@ class Replayer
   def self.execute_all_in file
     lines = read_lines(file)
 
-    result = {}
-    line_number = 1
-    lines.map do |line|
-      begin
-        r = eval(line)
-        result[line_number] = {error: false, result: r}
-      rescue StandardError => e
-        result[line_number] = {error: true, result: e}
-      end
-      line_number = line_number + 1
-    end
-
-    result
+    execute_all(lines)
   end
 
   private
@@ -26,6 +14,21 @@ class Replayer
       result << "#{line}"
     end
     
+    result
+  end
+
+  def self.execute_all lines
+    result = {}
+    line_number = 1
+    lines.map do |line|
+      begin
+        r = eval(line)
+        result[line_number] = {error: false, result: r}
+      rescue StandardError => e
+        result[line_number] = {error: true, result: e}
+      end
+      line_number = line_number + 1
+    end
     result
   end
 
