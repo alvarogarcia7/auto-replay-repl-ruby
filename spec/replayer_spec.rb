5646d012f7435497a117c460548851dd91bf131f
Alvaro Garcia <derecerca@gmail.com>
2015-11-07 15:12:30 +0100

    green: parse a whole file with errors

--- spec/replayer_spec.rb
+++ spec/replayer_spec.rb
@@ -2,9 +2,15 @@ require_relative "../replayer.rb"
 
 RSpec.describe 'replayer' do
 
-  def to_s result
-  	result[:result] = result[:result].message
-  	result
+  def to_s results
+    if results.key? :result
+      results[:result] = results[:result].message
+    else
+      results.values.map { |result|
+          result[:result] = result[:result].message
+      }
+    end
+    results
   end
   it 'should execute and return a whole file' do
     expect(Replayer.execute_all_in('spec/repl2.log').all).to eq(
@@ -23,6 +29,15 @@ RSpec.describe 'replayer' do
   	line = Replayer.execute_all_in('spec/unexisting_variable.log').at 1
     expect(to_s(line)).to eq(
       {:error=>true, :result=>"undefined local variable or method `a' for Replayer:Class", :code => "1+a", :line => 1})
+  end
+
+  it 'should execute and return a whole error file' do
+    file = Replayer.execute_all_in('spec/unexisting_variable.log').all
+    expect(to_s(file)).to eq(
+      {
+        1=>{:error=>true, :result=>"undefined local variable or method `a' for Replayer:Class", :code => "1+a", :line => 1},
+        2=>{:error=>true, :result=>"undefined local variable or method `b' for Replayer:Class", :code => "9*b", :line => 2}
+      })
   end
 end
 
